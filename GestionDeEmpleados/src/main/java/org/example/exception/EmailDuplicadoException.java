294f54472bf5bb9dc3d69ed7bae29aa9d8af3a10
Emicora <emicoratolo2501@gmail.com>
2025-08-31 18:50:01 -0300

    crear tests y correccion de proyecto

--- GestionDeEmpleados/src/main/java/org/example/exception/EmailDuplicadoException.java
+++
@@ -1,5 +0,0 @@
-package org.example.exception;
-
-public class EmailDuplicadoException extends RuntimeException {
-    public EmailDuplicadoException(String message) { super(message); }
-}
--- GestionDeEmpleados/src/main/java/org/example/exception/RecursoNoEncontradoException.java
+++
@@ -1,5 +0,0 @@
-package org.example.exception;
-
-public class RecursoNoEncontradoException extends RuntimeException {
-    public RecursoNoEncontradoException(String message) { super(message); }
-}
